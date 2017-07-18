09d3f39cffbe39d90eabaed8b1c070337dc5002e
msnr <msnr@msnrnomacbook-pro.local>
2017-07-19 00:24:13 +0900

    second commit

--- SubPanelSample/SubPanel.swift
+++ SubPanelSample/SubPanel.swift
@@ -11,10 +11,15 @@ import UIKit
 class SubPanel : UIView {
     
     var str : NSString = "sample"
+    var isDisplay : Bool = true
     let textStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
     
     func initilize() {
         KvoModel.sharedInstance.addObserver(self, forKeyPath: "point", options: [.new], context: nil)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(SubPanel.tapges(_:)))
+        
+        self.addGestureRecognizer(tap)
     }
 
     
@@ -26,7 +31,7 @@ class SubPanel : UIView {
             ] as [NSAttributedStringKey: Any]
         
 
-        str.draw(in: CGRect(x: 20, y: 20, width: self.frame.width - 10, height: 50)
+        str.draw(in: CGRect(x: 20, y: 100, width: self.frame.width - 10, height: 50)
             , withAttributes: textFontAttributes)
     }
     
@@ -38,4 +43,17 @@ class SubPanel : UIView {
             self.setNeedsDisplay()
         }
     }
+    
+    @objc func tapges(_ sender: UITapGestureRecognizer) {
+        
+        isDisplay = !isDisplay
+        let sign : CGFloat = isDisplay ? 1 : -1
+        
+        UIView.animate(withDuration: 1, animations: {
+            self.center = CGPoint(x: self.center.x + (sign * (self.frame.width - 10)), y: self.center.y)
+        }, completion: nil)
+        
+    }
+
+
  }
